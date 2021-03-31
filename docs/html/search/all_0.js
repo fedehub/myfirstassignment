891bda70d339a116401c12a3fabdebd3b26566ca
federicohub <fedeunivers@gmail.com>
2021-03-31 12:13:54 +0200

    doc first atttempt

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
+  ['research_20track_20i_20_2d_20first_20assignment_1',['Research Track I - first assignment',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['research_20track_20i_20_2d_20first_20assignment_3',['Research Track I - first assignment',['../md__r_e_a_d_m_e.html',1,'']]]
+];
